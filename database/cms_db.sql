12591c72550fa1f655c0ee01f3e801c03449fa19
Md-Abdur-Rahman-07 <115738274+Md-Abdur-Rahman-07@users.noreply.github.com>
2025-01-25 00:29:18 +0600

    wow!

--- database/cms_db.sql
+++ database/cms_db.sql
@@ -1,21 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Feb 08, 2021 at 06:35 AM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 7.2.34
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `cms_db`
